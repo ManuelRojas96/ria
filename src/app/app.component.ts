e9f60285fe358d6ff6fdde6ab04e18fa958de911
Manuel Rojas <manuurojas96@gmail.com>
2025-03-18 00:57:15 -0300

    :wrench: Set up router and clean app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherViewComponent } from './components/weather-view/weather-view.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ria-test-app';
