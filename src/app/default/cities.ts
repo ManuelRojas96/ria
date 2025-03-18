1b2286b3f250fdf7aac8887eff909f93b5f6a44d
Manuel Rojas <manuurojas96@gmail.com>
2025-03-18 00:57:45 -0300

    :monocle_face: Add default data

---
+++ src/app/default/cities.ts
@@ -0,0 +1,19 @@
+import { City } from '../models/city';
+
+export const DEFAULT_CITIES: City[] = [
+  {
+    name: 'Rio de Janeiro',
+    lat: -22.9068,
+    lon: 43.1729,
+  },
+  {
+    name: 'Beijing',
+    lat: 39.9042,
+    lon: 116.4074,
+  },
+  {
+    name: 'Los Angeles',
+    lat: 34.0522,
+    lon: -118.2437,
+  },
+];
