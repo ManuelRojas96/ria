7724eaf538826f273ddb7864ab97002a81b177cb
Manuel Rojas <manuurojas96@gmail.com>
2025-03-18 19:14:35 -0300

    :art: Add dummy data to repo

---
+++ src/app/default/dummy-daily-data.ts
@@ -0,0 +1,37 @@
+export const DAILY_FORECAST = [
+  {
+    date: '2023-11-01T00:00:00',
+    description: 'Clear throughout the day.',
+    maxTemp: 27,
+    minTemp: 11,
+    iconId: 'c02d',
+  },
+  {
+    date: '2023-11-02T00:00:00',
+    description: 'Partly cloudy.',
+    maxTemp: 25,
+    minTemp: 13,
+    iconId: 'c02d',
+  },
+  {
+    date: '2023-11-03T00:00:00',
+    description: 'Rain showers.',
+    maxTemp: 22,
+    minTemp: 15,
+    iconId: 'c02d',
+  },
+  {
+    date: '2023-11-04T00:00:00',
+    description: 'Sunny.',
+    maxTemp: 28,
+    minTemp: 16,
+    iconId: 'c02d',
+  },
+  {
+    date: '2023-11-05T00:00:00',
+    description: 'Thunderstorms possible.',
+    maxTemp: 21,
+    minTemp: 14,
+    iconId: 'c02d',
+  },
+];
---
+++ src/app/default/dummy-hourly-data.ts
@@ -0,0 +1,38 @@
+export const HOURLY_DATA = [
+  {
+    timestamp: 1633024800,
+    temperature: 20,
+    rainChance: 0.1,
+    iconId: '01d',
+  },
+  {
+    timestamp: 1633024800,
+    temperature: 20,
+    rainChance: 0.1,
+    iconId: '03d',
+  },
+  {
+    timestamp: 1633024800,
+    temperature: 20,
+    rainChance: 0.1,
+    iconId: '10d',
+  },
+  {
+    timestamp: 1633024800,
+    temperature: 20,
+    rainChance: 0.1,
+    iconId: '01d',
+  },
+  {
+    timestamp: 1633024800,
+    temperature: 20,
+    rainChance: 0.1,
+    iconId: '03d',
+  },
+  {
+    timestamp: 1633024800,
+    temperature: 20,
+    rainChance: 0.1,
+    iconId: '10d',
+  },
+];
